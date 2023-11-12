6fa8666a8c84e8b5566069a774b19cf4a91e119f
Lecca Radu <radu.d.lecca@gmail.com>
2023-11-12 14:18:56 +0200

    Added real-time database

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAMmfoa_zhCr2SWpNDv1Y0PndPow09zqQc",
+  authDomain: "vpharmadatabase.firebaseapp.com",
+  projectId: "vpharmadatabase",
+  storageBucket: "vpharmadatabase.appspot.com",
+  messagingSenderId: "223466754303",
+  appId: "1:223466754303:web:1ed9d91971424e9c977cca",
+  measurementId: "G-J1SPR2JWK2"
+};
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,16 +35,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Virtual-Pharmacy' title`, () => {
+  it(`should have the 'Virtual_Pharmacy' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Virtual-Pharmacy');
+    expect(app.title).toEqual('Virtual_Pharmacy');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Virtual-Pharmacy');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Virtual_Pharmacy');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Virtual Pharmacy';
+  title = 'Virtual_Pharmacy';
 }
